7c3b125e1461f035bccf1b59074a4722aceb1fd5
gopaladhikari <gopuadks@gmail.com>
2026-02-24 12:56:42 +0545

    prototypes

--- javascript/oop/prototype.js
+++ javascript/oop/prototype.js
@@ -1,3 +1,5 @@
+// Prototypes: inheriting properties from parent objects
+
 const computer = {
   cpu: 12,
   ram: 8,
@@ -20,3 +22,30 @@ console.log("computers", computer.__proto__);
 console.log("macbook", macbook.__proto__);
 
 console.log("macbook cpu", Object.getPrototypeOf(macbook));
+
+function User() {}
+
+User.prototype.sayHi = function () {};
+
+const gopal = new User();
+const gopal2 = new User();
+
+console.log("gopal", Object.getPrototypeOf(gopal));
+console.log("gopal2", gopal2.__proto__);
+
+function Car() {}
+
+Car.prototype.honk = function (model) {
+  this.model = model;
+
+  console.log(this.model);
+};
+
+const toyota = new Car();
+const tesla = new Car();
+
+toyota.honk("toyota");
+tesla.honk("tesla");
+
+console.log(Car.prototype === Object.getPrototypeOf(toyota));
+console.log(Car.prototype === Object.getPrototypeOf(tesla));
