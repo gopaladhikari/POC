2fb23a28b75935ecd231d47e389781e6f2672d34
gopaladhikari <gopuadks@gmail.com>
2026-04-10 21:54:39 +0545

    dom-bom

---
+++ javascript/dom-and-bom/index.js
@@ -0,0 +1,10 @@
+// DOM  = html elements
+
+// BOM = browser things = height, width, scroll, location, navigator
+
+// js = js
+
+console.log("hello world");
+
+console.log(window);
+console.log(navigator.userAgent);
