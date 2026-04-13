c284c13fc0aab8a184f9918e6c01f8661e1dafb5
gopaladhikari <gopuadks@gmail.com>
2026-04-13 17:39:15 +0545

    strings

---
+++ python/strings.py
@@ -0,0 +1,29 @@
+name = "gOpal"
+
+nameLenth = len(name)
+
+nameShort = name[1:2]
+
+print(nameShort)  
+
+nameUpper = name.upper()
+
+print(nameUpper)
+
+nameLower = name.lower()
+
+print(nameLower)
+
+nameCapitalized = name.capitalize()
+
+print(nameCapitalized)
+
+print(name[1:4:2])
+
+print(name.endswith("opal"))
+
+print(name.startswith("g"))
+
+print(name.find("opal"))
+
+print(name.replace("g", "j"))
