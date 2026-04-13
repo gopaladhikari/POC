cba39df6ab5de87a91a29b312d2facde5e4e9bcb
gopaladhikari <gopuadks@gmail.com>
2026-04-13 17:23:59 +0545

    operators in python

---
+++ python/operators.py
@@ -0,0 +1,40 @@
+# Operators
+
+# Assignment Operators
+a = 10
+b = 20
+
+# Arithmetic Operators
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+h = a ** b
+i = a // b # floor division 10//3 = 3
+
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
+# Comparison Operators
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# Logical Operators
+x = True
+y = False
+z = x and y
+w = x or y
+
+print(z)
+print(w)
