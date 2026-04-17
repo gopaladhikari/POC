98424cc486195bc184a7c8a5e26bd897fb14a0e5
gopaladhikari <gopuadks@gmail.com>
2026-04-17 18:46:39 +0545

    nested models

---
+++ pydantic/nested-model/course-model.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Course(BaseModel):
+    id: int
+    name: str
+    modules: List[Module]
+
+
+class Module(BaseModel):
+    id: int
+    name: List[str]
+    lessons: List[Lesson]
+
+
+class Lesson(BaseModel):
+    id: int
+    topic: str
+
+
+Course.model_rebuild()
+Module.model_rebuild()
---
+++ pydantic/nested-model/nested-model.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+
+class Address(BaseModel):
+    street: str
+    city: str
+    postal_code: str
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    address: Address
+
+
+class Comment(BaseModel):
+    id: int
+    text: str
+    replies: Optional[List[Comment]] = None  # self referential
+
+
+Comment.model_rebuild()
+
+
+address = Address(street="123 Main St", city="Anytown", postal_code="12345")
+
+user = User(id=1, name="John Doe", address=address)
+
+comment = Comment(id=1, text="Hello World", replies=[Comment(id=2, text="Reply 1")])
+
+print(user)
+print(comment)
+print(address)
